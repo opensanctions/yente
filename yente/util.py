643eeb7f59cf6a4a158b205ac3cb14dff1a364dc
Friedrich Lindenberg <friedrich@pudo.org>
2022-01-19 11:57:22 +0100

    streaming CSV helper

--- yente/util.py
+++ yente/util.py
@@ -1,3 +1,4 @@
+import codecs
 from typing import Optional
 from datetime import datetime
 
@@ -5,6 +6,23 @@ from datetime import datetime
 class EntityRedirect(Exception):
     def __init__(self, canonical_id):
         self.canonical_id = canonical_id
+
+
+class AsyncTextReaderWrapper:
+    # from: https://github.com/MKuranowski/aiocsv/issues/2#issuecomment-706554973
+    def __init__(self, obj, encoding, errors="strict"):
+        self.obj = obj
+
+        decoder_factory = codecs.getincrementaldecoder(encoding)
+        self.decoder = decoder_factory(errors)
+
+    async def read(self, size):
+        raw_data = await self.obj.read(size)
+
+        if not raw_data:
+            return self.decoder.decode(b"", final=True)
+
+        return self.decoder.decode(raw_data, final=False)
 
 
 def match_prefix(prefix: str, *labels: Optional[str]):
