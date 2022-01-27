6734249e8fdad5f25b1e4f31052cde7218d4b4ad
Friedrich Lindenberg <friedrich@pudo.org>
2022-01-27 09:03:55 +0100

    test search filters and facets

--- tests/test_search.py
+++ tests/test_search.py
@@ -11,6 +11,49 @@ def test_search_putin():
     assert results[0]["id"] == "Q7747", results
 
 
+def test_search_no_query():
+    res = client.get("/search/default")
+    assert res.status_code == 200, res
+    results = res.json()["results"]
+    assert len(results) > 9, results
+
+
+def test_search_filter_schema_invalid():
+    res = client.get("/search/default?q=angela merkel&schema=Banana")
+    assert res.status_code == 400, res
+
+
+def test_search_filter_schema_remove():
+    res = client.get("/search/default?q=angela merkel&schema=Vessel")
+    assert res.status_code == 200, res
+    results = res.json()["results"]
+    assert len(results) == 0, results
+
+
+def test_search_filter_schema_keep():
+    res = client.get("/search/default?q=angela merkel&schema=Person")
+    assert res.status_code == 200, res
+    results = res.json()["results"]
+    assert len(results) > 0, results
+
+
+def test_search_filter_countries_remove():
+    res = client.get("/search/default?q=angela merkel&countries=ke")
+    assert res.status_code == 200, res
+    results = res.json()["results"]
+    assert len(results) == 0, results
+
+
+def test_search_facet_countries():
+    res = client.get("/search/default?q=angela merkel&countries=de")
+    assert res.status_code == 200, res
+    countries = res.json()["facets"]["countries"]
+    names = [c["name"] for c in countries["values"]]
+    assert "de" in names, names
+    assert "ke" not in names, names
+    assert "ru" not in names, names
+
+
 def test_search_putin_scope():
     res = client.get("/search/sanctions?q=vladimir putin")
     assert res.status_code == 200, res
