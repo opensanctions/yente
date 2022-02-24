d20e148841c7d2085a3d209f882cb7003d9f64e7
Friedrich Lindenberg <friedrich@pudo.org>
2022-02-24 12:15:15 +0100

    Bump version: 1.2.1 → 1.3.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md") as f:
 
 setup(
     name="yente",
-    version="1.2.1",
+    version="1.3.0",
     url="https://opensanctions.org/docs/api/",
     long_description=long_description,
     long_description_content_type="text/markdown",
