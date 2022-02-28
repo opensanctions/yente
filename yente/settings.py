bba503f77ec43e0323002a84701bdb0b32a13954
Friedrich Lindenberg <friedrich@pudo.org>
2022-02-28 10:59:37 +0100

    Bump version: 1.3.2 → 1.3.3

--- yente/settings.py
+++ yente/settings.py
@@ -10,7 +10,7 @@ def env_str(name: str, default: str) -> str:
     return default if value is None else value
 
 
-VERSION = "1.3.2"
+VERSION = "1.3.3"
 AUTHOR = "OpenSanctions"
 HOME_PAGE = "https://www.opensanctions.org"
 EMAIL = "info@opensanctions.org"
