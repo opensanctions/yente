b8efe8533170209511fbbee5e78644bac7f07cca
Friedrich Lindenberg <friedrich@pudo.org>
2022-04-07 09:14:29 +0200

    Overengineer match handling

--- tests/test_match.py
+++ tests/test_match.py
@@ -9,6 +9,34 @@ EXAMPLE = {
     },
 }
 
+ERMAKOV = {
+    "properties": {
+        "name": [
+            "ERMAKOV Valery Nikolaevich",
+            "Ermacov Valeryi Nycolaevych",
+            "Ermakov Valerij Nikolaevich",
+            "Ermakov Valerij Nikolaevič",
+            "Ermakov Valerijj Nikolaevich",
+            "Ermakov Valeriy Nikolaevich",
+            "Ermakov Valery Nykolaevych",
+            "Ermakov Valeryi Nykolaevych",
+            "Ermakov Valeryy Nikolaevich",
+            "Ermakov Valeryy Nykolaevych",
+            "Ermakov Valerȳĭ Nȳkolaevȳch",
+            "Iermakov Valerii Mykolaiovych",
+            "Jermakov Valerij Mikolajovich",
+            "Jermakov Valerij Mikolajovič",
+            "Jermakov Valerij Mykolajovyč",
+            "Yermakov Valerii Mykolaiovych",
+            "Yermakov Valerij Mykolajovych",
+            "Yermakov Valeriy Mykolayovych",
+            "Êrmakov Valerìj Mikolajovič",
+            "ЕРМАКОВ Валерий Николаевич",
+        ]
+    },
+    "schema": "Person",
+}
+
 
 def test_match_putin():
     query = {"queries": {"vv": EXAMPLE, "xx": EXAMPLE, "zz": EXAMPLE}}
@@ -21,3 +49,10 @@ def test_match_putin():
     assert res["total"]["value"] > 0, res["total"]
     res0 = res["results"][0]
     assert res0["id"] == "Q7747", res0
+
+
+def test_match_ermakov():
+    query = {"queries": {"ermakov": ERMAKOV}}
+    resp = client.post("/match/default", json=query)
+    assert resp.status_code == 200, resp.text
+    data = resp.json()
--- yente/search/queries.py
+++ yente/search/queries.py
@@ -1,5 +1,7 @@
 import logging
-from typing import Any, Dict, List, Union, Optional
+from pprint import pprint
+from normality import collapse_spaces
+from typing import Any, Dict, List, Set, Union, Optional
 from followthemoney.schema import Schema
 from followthemoney.proxy import EntityProxy
 from followthemoney.types import registry
@@ -38,28 +40,58 @@ def filter_query(
     return {"bool": {"filter": filterqs, "should": shoulds, "minimum_should_match": 1}}
 
 
-def entity_query(dataset: Dataset, entity: EntityProxy):
-    shoulds: List[Dict[str, Any]] = []
-    for prop, value in entity.itervalues():
-        if not prop.matchable:
-            continue
-        if prop.type in TEXT_TYPES:
-            is_name = prop.type == registry.name
+def names_query(entity: EntityProxy) -> List[Dict[str, Any]]:
+    names = entity.get_type_values(registry.name, matchable=True)
+    # When there are a limited number of names, try fuzzy matching in the
+    # index:
+    if len(names) < 5:
+        shoulds = []
+        for name in names:
             query = {
                 "match": {
-                    prop.type.group: {
-                        "query": value,
-                        "fuzziness": "AUTO" if is_name else 0,
+                    registry.name.group: {
+                        "query": name,
+                        "fuzziness": "AUTO",
                         "minimum_should_match": 1,
-                        "boost": 3.0 if is_name else 1.0,
+                        "boost": 3.0,
                     }
                 }
             }
+            shoulds.append(query)
+        return shoulds
+    # Deduplicate names before making them a match query. This is a hack to
+    # work around a low default for `query.bool.max_clause_count` in Elastic
+    # cloud. I'm not sure if overall it's an improvement to the query perf or
+    # recall on entity matching.
+    normalized_ = [collapse_spaces(n.lower()) for n in names]
+    normalized = set([n for n in normalized_ if n is not None])
+    # This query is non-fuzzy:
+    query = {
+        "match": {
+            registry.name.group: {
+                "query": " ".join(normalized),
+                "minimum_should_match": 1,
+                "boost": 3.0,
+            }
+        }
+    }
+    return [query]
+
+
+def entity_query(dataset: Dataset, entity: EntityProxy):
+    shoulds: List[Dict[str, Any]] = []
+    for prop, value in entity.itervalues():
+        if prop.type == registry.name or not prop.matchable:
+            continue
+        if prop.type in TEXT_TYPES:
+            query = {"match": {prop.type.group: {"query": value}}}
             shoulds.append(query)
         elif prop.type.group is not None:
             shoulds.append({"term": {prop.type.group: value}})
         else:
             shoulds.append({"match_phrase": {"text": value}})
+
+    shoulds.extend(names_query(entity))
     return filter_query(shoulds, dataset=dataset, schema=entity.schema)
 
 
@@ -77,10 +109,9 @@ def text_query(
         should = {
             "query_string": {
                 "query": query,
-                # "default_field": "text",
                 "fields": ["names^3", "text"],
                 "default_operator": "and",
-                "fuzziness": 2 if fuzzy else 0,
+                "fuzziness": "AUTO" if fuzzy else 0,
                 "lenient": fuzzy,
             }
         }
@@ -108,12 +139,6 @@ def facet_aggregations(fields: List[str] = []) -> Dict[str, Any]:
 def statement_query(
     dataset=Optional[Dataset], **kwargs: Optional[Union[str, bool]]
 ) -> Dict[str, Any]:
-    # dataset: Optional[str] = None,
-    # entity_id: Optional[str] = None,
-    # canonical_id: Optional[str] = None,
-    # prop: Optional[str] = None,
-    # value: Optional[str] = None,
-    # schema: Optional[str] = None,
     filters = []
     if dataset is not None:
         filters.append({"terms": {"dataset": dataset.source_names}})
