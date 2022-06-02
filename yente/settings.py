7bdb165e306ff1df9c7ce98665bc1e0e532b3a81
Friedrich Lindenberg <friedrich@pudo.org>
2022-06-02 16:55:04 +0200

    Bump version: 1.6.7 → 1.7.0

--- yente/settings.py
+++ yente/settings.py
@@ -13,7 +13,7 @@ def env_str(name: str, default: Optional[str] = None) -> Optional[str]:
     return default if value is None else value
 
 
-VERSION = "1.6.7"
+VERSION = "1.7.0"
 AUTHOR = "OpenSanctions"
 HOME_PAGE = "https://www.opensanctions.org"
 EMAIL = "info@opensanctions.org"
