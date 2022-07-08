f5ec7285813d6011ae8e135342e8edd9c62fcf3c
Friedrich Lindenberg <friedrich@pudo.org>
2022-07-08 08:24:58 +0200

    Add error handling to load testing script

--- examples/load_test.py
+++ examples/load_test.py
@@ -3,6 +3,7 @@ import random
 import logging
 import requests
 from urllib.parse import urljoin
+from requests import JSONDecodeError
 from concurrent.futures import ThreadPoolExecutor
 
 log = logging.getLogger("load_test")
@@ -63,15 +64,19 @@ def match_api():
     BATCH = {"queries": {"q1": EXAMPLE_1, "q2": EXAMPLE_2, "q3": EXAMPLE_3}}
     url = urljoin(HOST, "/match/sanctions")
     response = session.post(url, json=BATCH)
-    if response.status_code != 200:
-        log.error("Failed [%s]: %s", url, response.text)
+    if not response.ok:
+        log.error("Failed: %s (%s)", url, response.status_code)
         return
-    responses = response.json().get("responses")
-    log.info("Match: %s", len(BATCH))
+    try:
+        responses = response.json().get("responses")
+    except JSONDecodeError:
+        log.error("Invalid server response")
+        return
     for resp in responses.values():
         # print(list(resp.keys()))
         for result in resp.get("results", []):
             ENTITY_IDS.add(result["id"])
+            log.info("Match: %s", result["id"])
 
 
 def search_api():
@@ -81,9 +86,13 @@ def search_api():
     log.info("Query: %s (limit %d)", q, params["limit"])
     response = session.get(url, params=params)
     if not response.ok:
-        log.error("Failed: %s", url)
+        log.error("Failed: %s (%s)", url, response.status_code)
         return
-    data = response.json()
+    try:
+        data = response.json()
+    except JSONDecodeError:
+        log.error("Invalid server response")
+        return
     if "results" not in data:
         log.error("Invalid response: %r", data)
         return
@@ -95,7 +104,9 @@ def entity_api():
     entity_id = random.choice(list(ENTITY_IDS))
     url = urljoin(HOST, f"/entities/{entity_id}")
     log.info("Entity: %s", entity_id)
-    session.get(url)
+    response = session.get(url)
+    if not response.ok:
+        log.error("Failed: %s (%s)", url, response.status_code)
 
 
 APIS = [match_api, search_api, entity_api, entity_api]
