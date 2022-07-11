5aee66b002c1b614c039b3c7d381d69af688af00
Friedrich Lindenberg <friedrich@pudo.org>
2022-07-11 10:28:47 +0200

    Fix KYC tutorial links

--- yente/settings.py
+++ yente/settings.py
@@ -43,7 +43,7 @@ Further reading:
 
 * [Install and deployment](https://github.com/opensanctions/yente/blob/main/README.md)
 * Intro to the [entity data model](https://www.opensanctions.org/docs/entities/)
-* Tutorial: [Using the matching API to do KYC-style checks](/articles/2022-02-01-matching-api/)
+* Tutorial: [Using the matching API to do KYC-style checks](https://www.opensanctions.org/articles/2022-02-01-matching-api/)
 * [Data dictionary](https://opensanctions.org/reference/)
 * Advanced: [statement-based data model](https://www.opensanctions.org/docs/statements/)
 """
