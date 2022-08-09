111feeadd181bd274024471eab45037517e8cd11
Friedrich Lindenberg <friedrich@pudo.org>
2022-08-09 08:43:54 +0200

    Load collections list

--- yente/data/manifest.py
+++ yente/data/manifest.py
@@ -31,6 +31,7 @@ class ExternalManifest(BaseModel):
                 title=ds["title"],
                 version=iso_to_version(ds["last_export"]),
                 namespace=self.namespace,
+                collections=ds.get('collections', []),
                 datasets=datasets,
             )
             if dataset.name == self.scope:
