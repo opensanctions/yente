b4ad8b0407c08ae686388628875da57efd6f25ef
Friedrich Lindenberg <friedrich@pudo.org>
2022-08-29 18:07:52 +0200

    Bump version: 1.9.2 → 1.9.3

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md") as f:
 
 setup(
     name="yente",
-    version="1.9.2",
+    version="1.9.3",
     url="https://opensanctions.org/docs/api/",
     long_description=long_description,
     long_description_content_type="text/markdown",
--- yente/settings.py
+++ yente/settings.py
@@ -14,7 +14,7 @@ def env_str(name: str, default: Optional[str] = None) -> Optional[str]:
     return default if value is None else value
 
 
-VERSION = "1.9.2"
+VERSION = "1.9.3"
 AUTHOR = "OpenSanctions"
 HOME_PAGE = "https://www.opensanctions.org"
 EMAIL = "info@opensanctions.org"
