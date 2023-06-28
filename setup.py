11b5629ac17040db3043a6443859f63c89274de9
Friedrich Lindenberg <friedrich@pudo.org>
2023-06-28 15:57:59 +0200

    bump versions

--- setup.py
+++ setup.py
@@ -16,8 +16,8 @@ setup(
     packages=find_packages(exclude=["examples", "tests"]),
     namespace_packages=[],
     install_requires=[
-        "followthemoney==3.4.2",
-        "nomenklatura==3.0.0",
+        "followthemoney==3.4.3",
+        "nomenklatura==3.0.1",
         "asyncstdlib==3.10.8",
         "aiocron==1.8",
         "aiocsv==1.2.4",
