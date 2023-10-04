a64c6c8e8e8d98dd5d7cf496d21899d913d481e6
Friedrich Lindenberg <friedrich@pudo.org>
2023-10-04 15:58:35 +0200

    Bump version: 3.7.0 → 3.7.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md") as f:
 
 setup(
     name="yente",
-    version="3.7.0",
+    version="3.7.1",
     url="https://opensanctions.org/docs/api/",
     long_description=long_description,
     long_description_content_type="text/markdown",
