c48386da616b15cfbdc1fcd82047db245ec7afd4
Friedrich Lindenberg <friedrich@pudo.org>
2024-02-02 20:46:01 +0100

    up nk/rigour

--- setup.py
+++ setup.py
@@ -17,8 +17,8 @@ setup(
     namespace_packages=[],
     install_requires=[
         "followthemoney==3.5.9",
-        "nomenklatura==3.10.1",
-        "rigour==0.4.1",
+        "nomenklatura==3.10.3",
+        "rigour==0.4.3",
         "asyncstdlib==3.12.0",
         "aiocron==1.8",
         "aiocsv==1.2.5",
