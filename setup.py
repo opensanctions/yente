2d90ff73565c6739bea2d3bdb4d408824b642db9
Friedrich Lindenberg <friedrich@opensanctions.org>
2024-07-14 15:55:57 +0200

    configure context for opensearch a bit

--- setup.py
+++ setup.py
@@ -26,6 +26,7 @@ setup(
         "types-aiofiles>=23.1.0.4,<23.3",
         "aiohttp[speedups]==3.9.5",
         "elasticsearch[async]==8.14.0",
+        "opensearch-py[async]==2.6.0",
         "fastapi==0.111.0",
         "uvicorn[standard]==0.30.1",
         "httpx[http2]==0.27.0",
