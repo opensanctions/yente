ad1ab8cb6f32617f2f0b1d46da02d8b87db0fb40
Friedrich Lindenberg <friedrich@opensanctions.org>
2024-10-01 08:17:48 +0200

    Bump version: 4.0.0 → 4.1.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md") as f:
 
 setup(
     name="yente",
-    version="4.0.0",
+    version="4.1.0",
     url="https://opensanctions.org/docs/api/",
     long_description=long_description,
     long_description_content_type="text/markdown",
