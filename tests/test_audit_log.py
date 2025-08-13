20420110cbd005f6b744812c2ba3d08e2ca56478
Leon Handreke <leonh@ndreke.de>
2025-08-13 12:00:48 +0200

    Add more fields to audit log

--- tests/test_audit_log.py
+++ tests/test_audit_log.py
@@ -2,9 +2,14 @@ import pytest
 from yente.search.audit_log import (
     get_audit_log_index_name,
     AuditLogMessageType,
+    AuditLogReindexType,
     log_audit_message,
 )
 from yente.provider import SearchProvider
+
+TEST_INDEX_NAME = "test-index"
+TEST_DATASET = "test-dataset"
+TEST_DATASET_VERSION = "20250813-abc"
 
 
 @pytest.mark.asyncio
@@ -12,18 +17,36 @@ async def test_audit_log(search_provider: SearchProvider):
     await search_provider.delete_index(get_audit_log_index_name())
 
     await log_audit_message(
-        search_provider, "test-index-1", AuditLogMessageType.FULL_REINDEX_STARTED
-    )
-    await log_audit_message(
-        search_provider, "test-index-2", AuditLogMessageType.PARTIAL_REINDEX_COMPLETED
-    )
-    await log_audit_message(
-        search_provider, "test-index-1", AuditLogMessageType.FULL_REINDEX_FAILED
+        search_provider,
+        index=TEST_INDEX_NAME,
+        dataset=TEST_DATASET,
+        dataset_version=TEST_DATASET_VERSION,
+        message_type=AuditLogMessageType.REINDEX_STARTED,
+        reindex_type=AuditLogReindexType.FULL,
     )
     await log_audit_message(
         search_provider,
-        "test-index-4",
-        AuditLogMessageType.INDEX_ALIAS_ROLLOVER_COMPLETE,
+        index=TEST_INDEX_NAME,
+        dataset=TEST_DATASET,
+        dataset_version=TEST_DATASET_VERSION,
+        message_type=AuditLogMessageType.REINDEX_COMPLETED,
+        reindex_type=AuditLogReindexType.PARTIAL,
+    )
+    await log_audit_message(
+        search_provider,
+        index=TEST_INDEX_NAME,
+        dataset=TEST_DATASET,
+        dataset_version=TEST_DATASET_VERSION,
+        message_type=AuditLogMessageType.REINDEX_FAILED,
+        reindex_type=AuditLogReindexType.FULL,
+    )
+    await log_audit_message(
+        search_provider,
+        index=TEST_INDEX_NAME,
+        dataset=TEST_DATASET,
+        dataset_version=TEST_DATASET_VERSION,
+        message_type=AuditLogMessageType.INDEX_ALIAS_ROLLOVER_COMPLETE,
+        reindex_type=AuditLogReindexType.FULL,
     )
 
     await search_provider.refresh(get_audit_log_index_name())
@@ -35,8 +58,8 @@ async def test_audit_log(search_provider: SearchProvider):
         sort=[{"timestamp": {"order": "asc"}}],
     )
     oldest_hit = oldest_result["hits"]["hits"][0]["_source"]
-    assert oldest_hit["index"] == "test-index-1"
-    assert oldest_hit["message_type"] == AuditLogMessageType.FULL_REINDEX_STARTED
+    assert oldest_hit["index"] == TEST_INDEX_NAME
+    assert oldest_hit["message_type"] == AuditLogMessageType.REINDEX_STARTED
 
     newest_result = await search_provider.search(
         get_audit_log_index_name(),
@@ -45,7 +68,7 @@ async def test_audit_log(search_provider: SearchProvider):
         sort=[{"timestamp": {"order": "desc"}}],
     )
     newest_hit = newest_result["hits"]["hits"][0]["_source"]
-    assert newest_hit["index"] == "test-index-4"
+    assert newest_hit["index"] == TEST_INDEX_NAME
     assert (
         newest_hit["message_type"] == AuditLogMessageType.INDEX_ALIAS_ROLLOVER_COMPLETE
     )
