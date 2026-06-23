258c0a1fbcd9e7f70acdb113f19c28b7eac282b8
Leon Handreke <leon@opensanctions.org>
2026-06-23 15:02:32 +0200

    Bump version: 5.4.0 → 5.5.0

--- yente/settings.py
+++ yente/settings.py
@@ -24,7 +24,7 @@ def random_cron() -> str:
     return f"{random_minute} * * * *"
 
 
-VERSION = "5.4.0"
+VERSION = "5.5.0"
 AUTHOR = "OpenSanctions"
 HOME_PAGE = "https://www.opensanctions.org/"
 EMAIL = "info@opensanctions.org"
